d9a25f7e07dc8cd1a0d23921d98d3583359788f2
fN9ne <39432487+fN9ne@users.noreply.github.com>
2020-08-13 02:16:28 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -161,6 +161,15 @@ $(document).ready(function(){
 		$('body').removeClass('_lock')
 	})
 	//
+	$('.input-block_2').click(function(){
+		$('.block_2').addClass('_active')
+		$('.block_3').removeClass('_active')
+	})
+	$('.input-block_1').click(function(){
+		$('.block_2').removeClass('_active')
+		$('.block_3').addClass('_active')
+	})
+	//
 	function count01() {
 		var value = $(".notifiedTitle01").val().length
 		$('.currentLength01').html(value)
@@ -179,9 +188,30 @@ $(document).ready(function(){
 		var max = $('.notifiedTitle03').attr('maxlength')
 		$('.maxLength03').html(max)
 	}
+	function count04() {
+		var value = $(".notifiedTitle04").val().length
+		$('.currentLength04').html(value)
+		var max = $('.notifiedTitle04').attr('maxlength')
+		$('.maxLength04').html(max)
+	}
+	function count05() {
+		var value = $(".notifiedTitle05").val().length
+		$('.currentLength05').html(value)
+		var max = $('.notifiedTitle05').attr('maxlength')
+		$('.maxLength05').html(max)
+	}
+	function count06() {
+		var value = $('.XMLLink').val().length
+		$('.currentLength06').html(value)
+		var max = $('.XMLLink').attr('maxlength')
+		$('.maxLength06').html(max)
+	}
 	setInterval(count01, 100)
 	setInterval(count02, 100)
 	setInterval(count03, 100)
+	setInterval(count04, 100)
+	setInterval(count05, 100)
+	setInterval(count06, 100)
 	//
 	$('.preview-block__btn_1').click(function(){
 		$(this).addClass('_active')
@@ -192,3 +222,40 @@ $(document).ready(function(){
 		$('.preview-block__btn_1').removeClass('_active')
 	})
 })
+const pickr = Pickr.create({
+    el: '.color-picker',
+    theme: 'classic', // or 'monolith', or 'nano'
+    swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 1)',
+        'rgba(156, 39, 176, 1)',
+        'rgba(103, 58, 183, 1)',
+        'rgba(63, 81, 181, 1)',
+        'rgba(33, 150, 243, 1)',
+        'rgba(3, 169, 244, 1)',
+        'rgba(0, 188, 212, 1)',
+        'rgba(0, 150, 136, 1)',
+        'rgba(76, 175, 80, 1)',
+        'rgba(139, 195, 74, 1)',
+        'rgba(205, 220, 57, 1)',
+        'rgba(255, 235, 59, 1)',
+        'rgba(255, 193, 7, 1)'
+    ],
+    components: {
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: true,
+            hsla: true,
+            hsva: true,
+            cmyk: true,
+            input: true,
+            clear: true,
+            save: true
+        }
+    }
+});
